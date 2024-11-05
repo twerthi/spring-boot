ac44922541147e6d503b58be28be921cbc8f7d7d
twerthi <twerthi@hotmail.com>
2024-11-05 11:39:15 -0800

    Updating bash script

--- scripts/run-jar.sh
+++ scripts/run-jar.sh
@@ -3,5 +3,16 @@ filelocation=$(get_octopusvariable "Octopus.Action[Deploy Java Archive].Output.O
 
 echo "Octopus placed the deployed .jar here: $filelocation"
 
+echo "Checking to see if there is a running process for the jar"
+jarPid=$(ps aux | grep logging-l4j2.jar | awk '{ print $2 }')
+
+# Check to see if something was returned
+if [ -z "$jarPid" ]; then
+    echo "No running process found"
+else
+    echo "Killing process $jarPid"
+    kill -9 $jarPid
+fi
+
 # Execute the jar
 java -jar $filelocation >> /var/log/shawntest.log 2>&1 &
